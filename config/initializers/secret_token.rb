2be76b2bbb9082cc43f3db506d735f01d44974c8
SebaBM <sebabm@debian>
2013-07-11 20:06:17 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizzer::Application.config.secret_token = '41865a3c61c86b8f763db5f1a48d6aeb6a83d940fad34e1ff0b74186ca5316a271a7114afe2883dd2d23093f86913272dda60cecf38745d65084f8e0c2b949ad'
